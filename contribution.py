a680fd11251984b18fd3f255b5690543eb2bda3e
Sergio <sergio.paredes@est.fib.upc.edu>
2018-04-13 11:27:45 +0200

    Add support for asks and news

--- contribution.py
+++ contribution.py
@@ -1,17 +1,27 @@
+from enum import Enum
+
+
+class ContributionTypes(Enum):
+    ASK = 'ask'
+    NEW = 'new'
+
+
 class Contribution:
 
-    def __init__(self, title, url, text, time, username, contribution_id=None):
+    def __init__(self, title, url, text, time, username, kind, contribution_id=None):
         self.id = contribution_id
         self.title = title
         self.url = url
         self.text = text
         self.time = time
         self.username = username
+        self.kind = kind
 
     def save(self, repository):
-        sql_script = 'INSERT INTO contribution (title, url, \'text\', time, \'user\') VALUES (:title, :url, :text, :time, :username)'
+        sql_script = '''INSERT INTO contribution (title, url, \'text\', time, \'user\', kind) 
+                        VALUES (:title, :url, :text, :time, :username, :kind)'''
         contribution = {'title': self.title, 'url': self.url, 'text': self.text, 'time': self.time,
-                        'username': self.username}
+                        'username': self.username, 'kind': self.kind.value}
         self.id = repository.insert(sql_script, contribution)
 
     @staticmethod
@@ -23,9 +33,18 @@ class Contribution:
                          'text' TEXT,
                          time TIMESTAMP,
                          'user' TEXT NOT NULL,
+                         'kind' TEXT,
                          FOREIGN KEY('user') REFERENCES 'user' (username)
                          )'''
 
     @staticmethod
     def get_contributions(repository):
         return repository.list('SELECT * FROM contribution')
+
+    @staticmethod
+    def get_news(repository):
+        return repository.list('SELECT * FROM contribution WHERE kind = \'' + ContributionTypes.NEW.value + '\'')
+
+    @staticmethod
+    def get_asks(repository):
+        return repository.list('SELECT * FROM contribution WHERE kind = \'' + ContributionTypes.ASK.value + '\'')
