29c187814f18cb1fdae7feb27a17f7d146b06abe
Alex Contreras Barba <acontreras28@MacBook-Pro-de-Alex.local>
2018-04-16 00:03:53 +0200

    Added htm file

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
 
-app = Flask(__name__)
+import os
 
+from flask import Flask, logging, render_template
+
+from contribution import Contribution
+from persistence import Persistence
+from user import User
+
+app = Flask(__name__, static_folder='./static')
+
+pageSize = 30
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def home(p=1):
+    contributions = Contribution.get_news(repository)
+    offset = pageSize * (p-1)
+    contributions = contributions[offset:offset+pageSize]
+    p = p+1
+    return render_template('home.html', contributions=contributions, p = p)
 
 if __name__ == '__main__':
+    repository = Persistence(os.environ['DB_PATH'], logging.getLogger(__name__))
+    repository.init_db([User.get_table_creation(), Contribution.get_table_creation()])
     app.run()
