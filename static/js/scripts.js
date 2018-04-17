a8bd6ffb62134e75bb188c19f1cd10db995fe8f0
Joan <jsanchezgarcia13@gmail.com>
2018-04-17 19:39:10 +0200

    integration with Google

---
+++ static/js/scripts.js
@@ -0,0 +1,20 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function prepareGoogleSignIn() {
+    gapi.load('auth2', function () {
+        gapi.auth2.init({
+            client_id: '443234130566-cba0cgt2np2alo9e3jhpb7au9hmeptoh.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin'
+        }).attachClickHandler(document.getElementById('googleButton'), {}, onSignIn, function (error) {
+            alert(error);
+        })
+    });
+};
+
+prepareGoogleSignIn();
